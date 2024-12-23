91d57593f91d86d25407b625be921c0dca8825c0
Jeremy <jeremy.puglisi17@gmail.com>
2024-12-23 22:01:13 +0700

    Feat : invoice pdf header

--- helper/generateInvoicePDF.go
+++ helper/generateInvoicePDF.go
@@ -6,6 +6,8 @@ import (
 	"server/models"
 
 	"github.com/johnfercher/maroto/v2"
+	"github.com/johnfercher/maroto/v2/pkg/components/image"
+	"github.com/johnfercher/maroto/v2/pkg/components/line"
 	"github.com/johnfercher/maroto/v2/pkg/components/text"
 	"github.com/johnfercher/maroto/v2/pkg/config"
 	"github.com/johnfercher/maroto/v2/pkg/consts/align"
@@ -28,16 +30,17 @@ func GenerateInvoicePDF(invoice models.Invoice) {
 	m := maroto.New(cfg)
 
 	// Header
-	err := m.RegisterHeader(addHeader())
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	// err := m.RegisterHeader(addInvoiceHeader())
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
 
 	// Footer
-	if err = m.RegisterFooter(addFooter()); err != nil {
-		log.Fatal(err.Error())
-	}
+	// if err = m.RegisterFooter(addFooter()); err != nil {
+	// 	log.Fatal(err.Error())
+	// }
 
+	addInvoiceHeader(m)
 	// Invoice Number
 	addInvoiceNumber(m, invoice)
 	// Invoice Detail
@@ -64,14 +67,39 @@ func GenerateInvoicePDF(invoice models.Invoice) {
 	log.Println("PDF saved successfully")
 }
 
-func addInvoiceNumber(m core.Maroto, invoice models.Invoice) {
-	m.AddRow(5)
-	m.AddRow(10,
-		text.NewCol(6, "Reff No : "+invoice.InvoiceNumber,
+func addInvoiceHeader(m core.Maroto) {
+	m.AddRow(40, image.NewFromFileCol(12, "assets/logotext.png", props.Rect{
+		Percent: 100,
+		Center:  true,
+	}))
+	m.AddRow(5,
+		text.NewCol(12, "INTERNATIONAL SEA/AIR FREIGHT FORWARDERS",
 			props.Text{
-				Align: align.Left,
+				Align: align.Center,
 				Size:  12,
 				Style: fontstyle.Bold,
 			}))
+	m.AddRow(5,
+		text.NewCol(12, "(SEA-AIR, PROJECT CARGO SHIP & AIRCRAFT CHARTER)",
+			props.Text{
+				Align: align.Center,
+				Size:  12,
+				Style: fontstyle.Bold,
+			}))
+}
+
+func addInvoiceNumber(m core.Maroto, invoice models.Invoice) {
+	m.AddRow(10)
+	m.AddRow(30,
+		text.NewCol(6, "No Invoice : "+invoice.InvoiceNumber, props.Text{
+			Style: fontstyle.Bold,
+			Top:   10,
+		}),
+		image.NewFromFileCol(6, "assets/invoice-img.jpg", props.Rect{
+			Percent: 100,
+		}),
+	)
+	m.AddRow(5, line.NewCol(12, props.Line{SizePercent: 100}))
+
 	m.AddRow(5)
 }
